3b709a47a5eb01410bc063d475ea613f822b0c25
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-03-22 16:49:51 +1100

    tweaks

--- app/src/main/java/com/odiousapps/android/fixmystreetnet/Photos.java
+++ app/src/main/java/com/odiousapps/android/fixmystreetnet/Photos.java
@@ -1,15 +1,11 @@
 package com.odiousapps.android.fixmystreetnet;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
@@ -20,20 +16,18 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.TimeZone;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.FragmentActivity;
 
 public class Photos extends Activity
 {
 	private Report r = new Report();
-	private Common common;
 
 	private static final int REQUEST_IMAGE_CAPTURE1 = 1;
 	private static final int REQUEST_IMAGE_CAPTURE2 = 2;
 
-	private String closejpg, widejpg;
-	private Uri photoUri;
+	private String currentPhotoPath;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -41,11 +35,25 @@ public class Photos extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photos);
 
-		common = new Common(this);
-
 		Intent i = getIntent();
 		String json = (String) i.getSerializableExtra("report");
 		r = r.fromString(json);
+	}
+
+	private File createImageFile() throws IOException
+	{
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File image = File.createTempFile(imageFileName,  /* prefix */
+				".jpg",         /* suffix */
+				storageDir      /* directory */
+		);
+
+		// Save a file: path for use with ACTION_VIEW intents
+		currentPhotoPath = image.getAbsolutePath();
+		return image;
 	}
 
 	public void wideShotView(View v)
@@ -57,9 +65,23 @@ public class Photos extends Activity
 
 	public void closeShotView(View v)
 	{
-		Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-		if(galleryIntent.resolveActivity(getPackageManager()) != null)
-			startActivityForResult(galleryIntent, REQUEST_IMAGE_CAPTURE2);
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(getPackageManager()) == null)
+			return;
+
+		File photoFile = null;
+		try {
+			photoFile = createImageFile();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		if(photoFile != null)
+		{
+			Uri photoURI = FileProvider.getUriForFile(this,"com.odiousapps.android.fixmystreetnet.provider", photoFile);
+			takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE2);
+		}
 	}
 
 	@Override
@@ -73,9 +95,16 @@ public class Photos extends Activity
 			{
 				Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
 
+				File f = new File(getExternalFilesDir(""), "wide.jpg");
+				FileOutputStream fos = new FileOutputStream(f);
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 75, fos);
+				fos.flush();
+				fos.close();
+
+				r.wide = f.getAbsolutePath();
+
 				ImageView im = findViewById(R.id.imageView);
 				im.setImageBitmap(bitmap);
-
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -83,13 +112,10 @@ public class Photos extends Activity
 
 		if(requestCode == REQUEST_IMAGE_CAPTURE2 && resultCode == FragmentActivity.RESULT_OK)
 		{
-			try {
-				Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
-
-				ImageView im = findViewById(R.id.imageView2);
-				im.setImageBitmap(bitmap);
-
-			} catch (IOException e) {
+			try
+			{
+				Common.LogMessage(data.getData().getPath());
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
